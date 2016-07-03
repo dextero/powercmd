9a98b8a70811f796ecf9042a5af2355c240cf8c2
dextero <marcin@mradomski.pl>
2016-07-03 08:41:55 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='powercmd',
+      version='0.1',
+      description='A generic framework to build typesafe line-oriented command interpreters',
+      url='http://github.com/dextero/powercmd',
+      author='Marcin Radomski',
+      author_email='marcin@mradomski.pl',
+      license='MIT',
+      packages=['powercmd'],
+      zip_safe=True)
