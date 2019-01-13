7af40cafcd1d72b419bd93fffeccbd3058a4e595
Marcin Radomski <marcin@mradomski.pl>
2019-01-13 22:59:32 +0100

    test_completer fixes

--- powercmd/test/test_completer.py
+++ powercmd/test/test_completer.py
@@ -1,9 +1,10 @@
 import enum
 import unittest
-from typing import Callable, List, Mapping, Tuple
+from typing import List, Tuple
 
 from powercmd.completer import Completer
 from powercmd.command import Command
+from powercmd.commands_dict import CommandsDict
 from powercmd.test import test_utils
 
 from prompt_toolkit.completion import Completion
@@ -23,21 +24,23 @@ class TestCompleter(unittest.TestCase):
                     third: str):
             pass
 
-        completer = Completer({'test', Command('test', do_test)})
+        cmds = CommandsDict()
+        cmds['test'] = Command('test', do_test)
+        completer = Completer(cmds)
 
-        self.assertEqual(completer.get_completions(Document(text='t', cursor_position=1)),
-                         [Completion('test', start_position=-1, display_meta='')])
+        self.assertEqual(list(completer.get_completions(Document(text='t', cursor_position=1))),
+                         [Completion('test', start_position=-1)])
 
-        self.assertEqual(completer.get_completions(Document(text='test ', cursor_position=5)),
+        self.assertEqual(list(completer.get_completions(Document(text='test ', cursor_position=5))),
                          [Completion('arg_first', start_position=0, display_meta=str(str)),
                           Completion('arg_second', start_position=0, display_meta=str(int)),
                           Completion('third', start_position=0, display_meta=str(str))])
 
-        self.assertEqual(completer.get_completions(Document(text='test arg', cursor_position=8)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg', cursor_position=8))),
                          [Completion('arg_first', start_position=-3, display_meta=str(str)),
                           Completion('arg_second', start_position=-3, display_meta=str(int))])
 
-        self.assertEqual(completer.get_completions(Document(text='test arg_f', cursor_position=10)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg_f', cursor_position=10))),
                          [Completion('arg_first', start_position=-5, display_meta=str(str))])
 
     def test_complete_enum(self):
@@ -45,9 +48,11 @@ class TestCompleter(unittest.TestCase):
                     arg: TestEnum):
             pass
 
-        completer = Completer({'test', Command('test', do_test)})
+        cmds = CommandsDict()
+        cmds['test'] = Command('test', do_test)
+        completer = Completer(cmds)
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=', cursor_position=9)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=', cursor_position=9))),
                          [Completion('First', start_position=0, display_meta=str(str)),
                           Completion('Second', start_position=0, display_meta=str(str))])
 
@@ -56,13 +61,15 @@ class TestCompleter(unittest.TestCase):
                     arg: List[TestEnum]):
             pass
 
-        completer = Completer({'test', Command('test', do_test)})
+        cmds = CommandsDict()
+        cmds['test'] = Command('test', do_test)
+        completer = Completer(cmds)
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=', cursor_position=9)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=', cursor_position=9))),
                          [Completion('First', start_position=0, display_meta=str(str)),
                           Completion('Second', start_position=0, display_meta=str(str))])
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=First,', cursor_position=14)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=First,', cursor_position=14))),
                          [Completion('First', start_position=0, display_meta=str(str)),
                           Completion('Second', start_position=0, display_meta=str(str))])
 
@@ -75,17 +82,19 @@ class TestCompleter(unittest.TestCase):
                     arg: Tuple[TestEnum, TestEnum2]):
             pass
 
-        completer = Completer({'test', Command('test', do_test)})
+        cmds = CommandsDict()
+        cmds['test'] = Command('test', do_test)
+        completer = Completer(cmds)
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=', cursor_position=9)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=', cursor_position=9))),
                          [Completion('First', start_position=0, display_meta=str(str)),
                           Completion('Second', start_position=0, display_meta=str(str))])
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=First,', cursor_position=14)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=First,', cursor_position=14))),
                          [Completion('A', start_position=0, display_meta=str(str)),
                           Completion('B', start_position=0, display_meta=str(str))])
 
-        self.assertEqual(completer.get_completions(Document(text='test arg=First,A,', cursor_position=16)),
+        self.assertEqual(list(completer.get_completions(Document(text='test arg=First,A,', cursor_position=16))),
                          [])
 
     def test_complete_custom_completer_legacy(self):
@@ -103,12 +112,14 @@ class TestCompleter(unittest.TestCase):
                     arg: TestType):
             pass
 
-        completer = Completer({'test', Command('test', do_test)})
+        cmds = CommandsDict()
+        cmds['test'] = Command('test', do_test)
+        completer = Completer(cmds)
 
         with TestType.powercmd_complete.expect_no_calls():
-            self.assertEqual(completer.get_completions(Document(text='test arg', cursor_position=8))
+            self.assertEqual(list(completer.get_completions(Document(text='test arg', cursor_position=8)))
                              [Completion('arg', start_position=-3, display_meta=str(TestType))])
 
         with TestType.powercmd_complete.expect_call('c'):
-            self.assertEqual(completer.get_completions(Document(text='test arg=', cursor_position=9))
+            self.assertEqual(list(completer.get_completions(Document(text='test arg=', cursor_position=9)))
                              [Completion('complete', start_position=0, display_meta=str(TestType))])
