9edbd7c520b830ca187fdeb894e0e8641fb4d3d3
Marcin Radomski <marcin@mradomski.pl>
2019-01-14 00:06:58 +0100

    Update requirements and version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='powercmd',
-      version='0.1',
+      version='0.2',
       description='A generic framework to build typesafe line-oriented command interpreters',
       url='http://github.com/dextero/powercmd',
       author='Marcin Radomski',
       author_email='marcin@mradomski.pl',
       license='MIT',
       packages=['powercmd'],
-      zip_safe=True)
+      zip_safe=True,
+      install_reqiures=['prompt_toolkit >= 2.0'])
