6e48e735a3338ad9dc294acd31b6b680261f4822
Marcin Radomski <m.radomski@avsystem.com>
2019-08-05 19:20:56 +0200

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='powercmd',
-      version='0.2',
+      version='0.3',
       description='A generic framework to build typesafe line-oriented command interpreters',
       url='http://github.com/dextero/powercmd',
       author='Marcin Radomski',
