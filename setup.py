a1e9d6421c9514bc4211c376299f6c6e3c2fde45
Marcin Radomski <m.radomski@avsystem.com>
2020-03-15 13:35:13 +0100

    Fix dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='powercmd',
-      version='0.3.1',
+      version='0.3.2',
       description='A generic framework to build typesafe line-oriented command interpreters',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -17,4 +17,4 @@ setup(name='powercmd',
       license='MIT',
       packages=['powercmd'],
       zip_safe=True,
-      install_reqiures=['prompt_toolkit >= 2.0'])
+      install_requires=['prompt_toolkit >= 2.0'])
