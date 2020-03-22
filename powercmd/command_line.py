be1e6feb35c9d6c61b3b6289f985c9bb508e4916
Marcin Radomski <m.radomski@avsystem.com>
2020-03-22 17:00:52 +0100

    CommandLine: add .args

--- powercmd/command_line.py
+++ powercmd/command_line.py
@@ -6,6 +6,12 @@ import collections
 import re
 import shlex
 
+from typing import Mapping, Sequence
+
+
+NamedArg = collections.namedtuple('NamedArg', ['name', 'value'])
+PositionalArg = collections.namedtuple('PositionalArg', ['value'])
+
 
 class CommandLine:
     """
@@ -22,29 +28,33 @@ class CommandLine:
         # words = cmdline.split()
 
         self.command = words[0] if words else ''
-        self.named_args = collections.OrderedDict()
-        self.free_args = []
+        self.args = []
 
         for word in words[1:]:
             if re.match(r'^[a-zA-Z0-9_]+=', word):
                 name, value = word.split('=', maxsplit=1)
                 if name in self.named_args:
                     raise ValueError('multiple values for key: %s' % (name,))
-                self.named_args[name] = value
+                self.args.append(NamedArg(name, value))
             else:
-                self.free_args.append(word)
+                self.args.append(PositionalArg(word))
 
     def __eq__(self, other):
-        return ((self.command, self.named_args, self.free_args)
-                == (other.command, other.named_args, other.free_args))
+        return ((self.command, self.args) == (other.command, other.args))
 
     def __str__(self):
-        return ('command = %s\nnamed_args (%d):\n%s\nfree_args (%d):\n%s'
+        return ('command = %s\nargs (%d):\n%s'
                 % (self.command,
-                   len(self.named_args),
-                   '\n'.join('  %s=%s' % kv for kv in self.named_args.items()),
-                   len(self.free_args),
-                   '\n'.join('  %s' % x for x in self.free_args)))
+                   len(self.args),
+                   '\n'.join('  %s' % x for x in self.args)))
+
+    @property
+    def named_args(self) -> Mapping[str, str]:
+        return {arg.name: arg.value for arg in self.args if isinstance(arg, NamedArg)}
+
+    @property
+    def free_args(self) -> Sequence[str]:
+        return [arg.value for arg in self.args if isinstance(arg, PositionalArg)]
 
     def __repr__(self):
         return ('CommandLine(command=%s,named_args=%s,free_args=%s)'
